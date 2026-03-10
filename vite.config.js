e2cbaa1598ff07374c912adcacc9ef78b5df79c8
Quy <quy@broad.dev>
2026-03-10 13:36:51 -0400

    copy assets to dist
    when you want to access  them without a web server running

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+import { copyFileSync, cpSync, mkdirSync } from "fs";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default {
-  plugins: [cssInjectedByJsPlugin()],
+  plugins: [
+    cssInjectedByJsPlugin(),
+    {
+      name: "copy-public-to-dist",
+      closeBundle() {
+        cpSync("public/assets", "dist/assets", { recursive: true });
+        mkdirSync("dist/config", { recursive: true });
+        copyFileSync("public/config/site-config.json", "dist/config/site-config.json");
+      },
+    },
+  ],
   build: {
     lib: {
       entry: "src/index.js",
